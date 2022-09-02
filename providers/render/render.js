f54e546cff4b17a1046049eafa6ff2c117ccc731
Simen Daehlin <sdaehlin@luno.com>
2022-09-02 13:06:03 +0100

    Added render.js

--- providers/render/render.js
+++ providers/render/render.js
@@ -1,30 +1,33 @@
-const { resolve, path } = require(`path`);
-const { copyFile, constants, chalk } = require(`node:fs/promises`);
+const { resolve } = require(`path`);
 const { Liquid } = require(`liquidjs`);
-const { config, spinner } = require(`../../utils`);
+const { config, spinner, chalk } = require(`../../utils`);
+const fs = require(`fs-extra`);
+
 const liquidEngine = new Liquid({
-	root: resolve(__dirname, `templates`),
-	extname: `.liquid`
+  root: resolve(__dirname, `templates`),
+  extname: `.liquid`
 });
 
-const renderconfig = async () => {
-	try {
-		const template = liquidEngine.renderFileSync(`render`, {
-			name: config.projectName,
-			env: config.env,
-			nodeVersion: +process.version.match(/^v(\d+\.\d+)/)[1]
-		});
-		await copyFile(
-			`${config.providersDir}/${config.provider}/templates/${config.provider}.liquid`,
-			`${config.outDir}/${config.provider}.yml`
-		);
-		spinner.stopAndPersist({
-			symbol: `🚀`,
-			text: ` Added ${chalk.bold.green(
-				config.provider.toUpperCase()
-			)} configuration file to project \n`
-		});
-	} catch (error) {}
+const renderConfig = async () => {
+  try {
+    const template = liquidEngine.renderFileSync(`render`, {
+      name: config.projectName,
+      env: config.env,
+      nodeVersion: +process.version.match(/^v(\d+\.\d+)/)[1]
+    });
+    const file = fs.createWriteStream(`${config.outDir}/render.yaml`);
+    file.write(template);
+    file.end();
+
+    await spinner.stopAndPersist({
+      symbol: `🚀`,
+      text: ` Added ${chalk.bold.green(
+        config.provider.toUpperCase()
+      )} configuration file to project \n`
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = { renderconfig };
+module.exports = { renderConfig };
