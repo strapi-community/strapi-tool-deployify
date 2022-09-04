78388ae76a442d041bee17bfdb95ed6dd62d78dc
Simen Daehlin <sdaehlin@luno.com>
2022-09-04 07:10:36 +0100

    fix(apikeys.js): fixing #3 - assumed unix not considering windows
    Fixed the issue where we assumed using unix ~ is not used in windows. This has now been fixed using
    os.homedir
    
    fix #3

--- providers/heroku/apiKey.js
+++ providers/heroku/apiKey.js
@@ -1,14 +1,23 @@
+const os = require(`os`);
 const shell = require(`shelljs`);
 const { setConfig } = require(`../../utils/config`);
+const { spinner } = require(`../../utils/config`);
 
 const getApiKey = async () => {
-  const apiToken = await shell
-    .cat(`~/.netrc `)
-    .grep(
-      `[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}`
-    )
-    .substring(11, 47);
-  setConfig({ apiToken });
+  try {
+    const apiToken = await shell
+      .cat(`${os.homedir()}/.netrc `)
+      .grep(
+        `[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}`
+      )
+      .substring(11, 47);
+    setConfig({ apiToken });
+  } catch (error) {
+    spinner.stopAndPersist({
+      symbol: `❌`,
+      text: `Unable to get API key from Heroku. Please make sure you are logged in to Heroku.`
+    });
+  }
 };
 
 module.exports = { getApiKey };
