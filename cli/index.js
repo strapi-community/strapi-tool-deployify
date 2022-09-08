1ec9c8ce4fe9912a9c82fa4907c4c1d2efd68c8e
Simen Daehlin <sdaehlin@luno.com>
2022-09-08 14:21:55 +0100

    fix(index.js): missing import of outputs

--- cli/index.js
+++ cli/index.js
@@ -1,7 +1,9 @@
 const { getArgs } = require(`./args`);
 const commands = require(`./commands`);
+const outputs = require(`./outputs`);
 
 module.exports = {
   getArgs,
-  commands
+  commands,
+  outputs
 };
